998df97473d5d346ee09323bd47bd3aed3936df3
aishaadeliaw <aishaadeliaw@student.unimelb.edu.au>
2025-11-02 07:53:24 +0000

    Add utilities/tab2.py (auto-commit for PR)

---
+++ utilities/tab2.py
@@ -0,0 +1,32 @@
+def app2()
+    st.title("Ask Questions")
+
+    api_key = os.environ.get("OPENAI_API_KEY")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt = st.chat_input("What is up?")
+
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            response = answer_query_with_rag(
+                collection=st.session_state.collection,
+                query=prompt,
+                report=st.session_state.report,
+                csv=st.session_state.clean_csv,
+                input_list=st.session_state.input_list,
+                n_results=3
+            )
+            st.markdown(response)
+        
+        st.session_state.messages.append({"role": "assistant", "content": response})
